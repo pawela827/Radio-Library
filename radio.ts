df6a91dc069f7fdb86f8324d59c0339a5d72acd0
pawela827 <101503059+pawela827@users.noreply.github.com>
2026-09-22 17:25:30 +0200

    Update radio.ts

--- radio.ts
+++ radio.ts
@@ -15,7 +15,7 @@ enum RadioPacketProperty {
  * Communicate data using radio packets
  */
 //% color=#E3008C weight=96 icon="\uf012" groups='["Group", "Broadcast", "Send", "Receive"]'
-namespace radio {
+namespace customradio {
 
     // keep in sync with CODAL
     const RADIO_MAX_PACKET_SIZE = 32;
